42a3b6579ff6db8c9be9eb10cedc4fc751d6b868
Damiano Colucci <dcolucci@c2r1p5.42firenze.it>
2023-07-11 15:43:54 +0200

    last_norminette prepush

--- srcs/utils/utils_free.c
+++ srcs/utils/utils_free.c
@@ -6,7 +6,7 @@
 /*   By: dcolucci <dcolucci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 18:10:03 by dcolucci          #+#    #+#             */
-/*   Updated: 2023/07/11 15:03:16 by dcolucci         ###   ########.fr       */
+/*   Updated: 2023/07/11 15:42:06 by dcolucci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,10 @@ void	ft_free_mat(char **mat)
 	free(mat);
 }
 
-void	ft_free_exit(char *error, char *str)
-{
-	printf("\033[31m%s\033[0m\n", error);
-	free(str);
-	exit(0);
-}
-
 void	ft_exit(char *error, t_program *p)
 {
 	printf("\033[31m%s\033[0m\n", error);
-	if(p->map)
+	if (p->map)
 		ft_free_mat(p->map);
 	if (p->file)
 		ft_free_mat(p->file);
